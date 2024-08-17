cce45abad12fbc215ed229c3ba96718bdbb30f12
Suranjan kumar <ksuranjan951@gmail.com>
2024-08-17 20:25:24 +0530

    ArrayDeque

---
+++ ArrayDequeExample.java
@@ -0,0 +1,9 @@
+import java.util.ArrayDeque;
+public class ArrayDequeExample {
+    public static void main(String[] args) {
+        ArrayDeque ad = new ArrayDeque();
+        ad.add(100);
+        ad.add(300);
+        System.out.println(ad);
+    }
+}
